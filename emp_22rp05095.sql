8f09edac3a96a4c8712a31239f2c485dadcfe4a2
planner <aimeishimwe330@gmail.com>
2024-08-21 22:59:56 +0200

    Add files via upload

---
+++ emp_22rp05095.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2024 at 10:42 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `emp_22rp05095`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee1`
+--
+
+CREATE TABLE `employee1` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` int(30) NOT NULL,
+  `email` int(20) NOT NULL,
+  `password` int(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empploye2`
+--
+
+CREATE TABLE `empploye2` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(29) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `position` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employee1`
+--
+ALTER TABLE `employee1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `empploye2`
+--
+ALTER TABLE `empploye2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `empploye2`
+--
+ALTER TABLE `empploye2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
